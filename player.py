fd18be9654cc6dc88137e1a9812812299f410a4b
FlyinSpyder <shane.provine@gmail.com>
2025-10-23 19:06:38 -0400

    added cooldown and strafe

--- player.py
+++ player.py
@@ -1,13 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOT_COOLDOWN
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.shots = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -38,15 +38,28 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        if keys[pygame.K_e]:
+            self.strafe(dt)
+        if keys[pygame.K_q]:
+            self.strafe(-dt)
+        if self.timer > 0:
+            self.timer -= dt
         #print(self.position)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
+    def strafe(self, dt):
+        right = pygame.Vector2(0, 1).rotate(self.rotation+90)
+        self.position += right * PLAYER_SPEED * dt
+    
     def shoot(self):
-        x = self.position[0]
-        y = self.position[1]
-        self.shots += 1
-        velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
-        Shot(x, y, velocity)
+        if self.timer > 0:
+            pass
+        else:
+            x = self.position[0]
+            y = self.position[1]
+            velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+            Shot(x, y, velocity)
+            self.timer = PLAYER_SHOT_COOLDOWN
