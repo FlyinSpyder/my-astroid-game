a2a10e153ca58c74745ac258c84e70d0550791f5
FlyinSpyder <shane.provine@gmail.com>
2025-10-22 23:33:48 -0400

    added player class

--- main.py
+++ main.py
@@ -3,20 +3,27 @@
 #throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_limit = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     black = pygame.Color(0, 0, 0)
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #makes the X button work to close the program
                 return
-        screen.fill(black)
+        screen.fill(black) 
+        player1.draw(screen)
         pygame.display.flip()
+        fps_limit.tick(60)
+        dt = fps_limit.tick(60)/1000
 
 if __name__ == "__main__":
     main()
