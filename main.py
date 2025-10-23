d4eaaeabc108db31107b865a9420270eb70ecf95
FlyinSpyder <shane.provine@gmail.com>
2025-10-23 18:49:55 -0400

    player can now shoot

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from constants import *
 from player import Player
 from asteroid import *
 from asteroidfield import *
+from shot import Shot
 
 def main():
     pygame.init()
@@ -20,10 +21,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     #add Player to updateable and drawable groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (updatable, drawable, asteroids)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable,drawable, shots)
 
     black = pygame.Color(0, 0, 0)
     player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
---
+++ shot.py
@@ -0,0 +1,25 @@
+from constants import SHOT_RADIUS
+from circleshape import *
+import pygame
+
+class Shot(CircleShape):
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y, SHOT_RADIUS)
+        self.velocity=velocity
+
+    def draw(self, screen):
+        white = (255, 255, 255)
+        pygame.draw.circle(
+            screen,
+            white, 
+            self.position,
+            self.radius,
+            width=2, 
+            draw_top_right = False,
+            draw_top_left = False,
+            draw_bottom_left = False,
+            draw_bottom_right = False,
+        )
+    
+    def update(self, dt):
+        self.position += self.velocity * dt
